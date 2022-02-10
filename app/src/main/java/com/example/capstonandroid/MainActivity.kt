dbd9673789f70f11498d93b8ef17d1dfc12ac21e
jangjaehyeon <jja33705@gmail.com>
2022-02-10 16:07:36 +0900

    layout_version_1

--- app/src/main/java/com/example/capstonandroid/MainActivity.kt
+++ app/src/main/java/com/example/capstonandroid/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.capstonandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.capstonandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 바텀 네비게이션에서 선택한 메뉴 아이디에 따라 표시할 화면 분기처리
+        binding.bottomNav.setOnItemSelectedListener {
+            println(it.itemId)
+            when (it.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+                }
+                R.id.trackFragment -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, TrackFragment()).commit()
+                }
+                R.id.recordActivity -> {
+                    val intent: Intent = Intent(this, RecordActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener false
+                }
+                R.id.meFragment -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, MeFragment()).commit()
+                }
+            }
+            true
+        }
+
+        // 처음 들어왔을때는 homefragment
+        binding.bottomNav.selectedItemId = R.id.homeFragment
+    }
+//    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
