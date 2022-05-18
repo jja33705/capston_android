8cfae8127dced91be3c15a2b18a13b5850cbee25
HDY <1901322@g.yju.ac.kr>
2022-05-18 14:23:15 +0900

    5/18 nice

--- app/src/main/java/com/example/capstonandroid/BadgeDialog.kt
+++ app/src/main/java/com/example/capstonandroid/BadgeDialog.kt
@@ -103,6 +103,7 @@ class BadgeDialog(
                     if(btn_0==0){
                         binding.set.isEnabled = false
                         binding.set.text = "未取得"
+                        binding.badgeImage.setImageResource(R.drawable.start_g)
                     }
                 }
                 else if(badgeType=="1"||badgeType=="2"||badgeType=="3"){
@@ -124,7 +125,9 @@ class BadgeDialog(
                                 binding.title.text = "高度初心者"
                                 binding.content.text = "どんどん慣れますよ"
                                 binding.number.text = "${myAltitude}m / 10000m"
+                                binding.badgeImage.setImageResource(R.drawable.altitude1)
                                 if(btn_1==0){
+                                    binding.badgeImage.setImageResource(R.drawable.altitude1_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -132,7 +135,9 @@ class BadgeDialog(
                                 binding.title.text = "高度中級者"
                                 binding.content.text = "結構慣れましたね！"
                                 binding.number.text = "${myAltitude}m / 20000m"
+                                binding.badgeImage.setImageResource(R.drawable.altitude2)
                                 if(btn_2==0){
+                                    binding.badgeImage.setImageResource(R.drawable.altitude2_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -140,7 +145,9 @@ class BadgeDialog(
                                 binding.title.text = "高度マスター"
                                 binding.content.text = "高度マスターになりました！"
                                 binding.number.text = "${myAltitude}m / 30000m"
+                                binding.badgeImage.setImageResource(R.drawable.altitude3)
                                 if(btn_3==0){
+                                    binding.badgeImage.setImageResource(R.drawable.altitude3_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -167,7 +174,9 @@ class BadgeDialog(
                                 binding.title.text = "サイクリング初心者"
                                 binding.content.text = "どんどん慣れますよ"
                                 binding.number.text = "${myBikeDistance}km / 1000km"
+                                binding.badgeImage.setImageResource(R.drawable.bike1)
                                 if(btn_4==0){
+                                    binding.badgeImage.setImageResource(R.drawable.bike1_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -175,7 +184,9 @@ class BadgeDialog(
                                 binding.title.text = "サイクリング中級者"
                                 binding.content.text = "結構慣れましたね！"
                                 binding.number.text = "${myBikeDistance}km / 5000km"
+                                binding.badgeImage.setImageResource(R.drawable.bike2)
                                 if(btn_5==0){
+                                    binding.badgeImage.setImageResource(R.drawable.bike2_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -183,7 +194,9 @@ class BadgeDialog(
                                 binding.title.text = "サイクリングマスター"
                                 binding.content.text = "サイクリングマスターになりました！"
                                 binding.number.text = "${myBikeDistance}km / 10000km"
+                                binding.badgeImage.setImageResource(R.drawable.bike3)
                                 if(btn_6==0){
+                                    binding.badgeImage.setImageResource(R.drawable.bike3_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -208,7 +221,9 @@ class BadgeDialog(
                                 binding.title.text = "ランニング初心者"
                                 binding.content.text = "どんどん慣れますよ"
                                 binding.number.text = "${myRunDistance}km / 100km"
+                                binding.badgeImage.setImageResource(R.drawable.run1)
                                 if(btn_7==0){
+                                    binding.badgeImage.setImageResource(R.drawable.run1_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -216,7 +231,9 @@ class BadgeDialog(
                                 binding.title.text = "ランニング中級者"
                                 binding.content.text = "結構慣れましたね！"
                                 binding.number.text = "${myRunDistance}km / 500km"
+                                binding.badgeImage.setImageResource(R.drawable.run2)
                                 if(btn_8==0){
+                                    binding.badgeImage.setImageResource(R.drawable.run2_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -224,7 +241,9 @@ class BadgeDialog(
                                 binding.title.text = "ランニングマスター"
                                 binding.content.text = "ランニングマスターになりました！"
                                 binding.number.text = "${myRunDistance}km / 1000km"
+                                binding.badgeImage.setImageResource(R.drawable.run3)
                                 if(btn_9==0){
+                                    binding.badgeImage.setImageResource(R.drawable.run3_g)
                                     binding.set.isEnabled = false
                                     binding.set.text = "未取得"
                                 }
@@ -244,7 +263,9 @@ class BadgeDialog(
                                     binding.title.text = "コースメーカ初心者"
                                     binding.content.text = "どんどん慣れますよ"
                                     binding.number.text = "${myTrackCount}回 / 3回"
+                                    binding.badgeImage.setImageResource(R.drawable.track1)
                                     if(btn_10==0){
+                                        binding.badgeImage.setImageResource(R.drawable.track1_g)
                                         binding.set.isEnabled = false
                                         binding.set.text = "未取得"
                                     }
@@ -252,7 +273,9 @@ class BadgeDialog(
                                     binding.title.text = "コースメーカ中級者"
                                     binding.content.text = "結構慣れましたね！"
                                     binding.number.text = "${myTrackCount}回 / 20回"
+                                    binding.badgeImage.setImageResource(R.drawable.track2)
                                     if(btn_11==0){
+                                        binding.badgeImage.setImageResource(R.drawable.track2_g)
                                         binding.set.isEnabled = false
                                         binding.set.text = "未取得"
                                     }
@@ -260,7 +283,9 @@ class BadgeDialog(
                                     binding.title.text = "コースメーカマスター"
                                     binding.content.text = "コースメーカマスターになりました！"
                                     binding.number.text = "${myTrackCount}回 / 50回"
+                                    binding.badgeImage.setImageResource(R.drawable.track3)
                                     if(btn_12==0){
+                                        binding.badgeImage.setImageResource(R.drawable.track3_g)
                                         binding.set.isEnabled = false
                                         binding.set.text = "未取得"
                                     }
